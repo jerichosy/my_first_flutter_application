1ce355055f1cab62969c446daf68625b193a5172
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2024-08-16 03:09:10 +0800

    Implement favorites page

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');  // fail-fast principle: explictly crash (in dev) instead of having silent/undetected error go to prod
@@ -182,6 +182,36 @@ class BigCard extends StatelessWidget {
           semanticsLabel: "${pair.first} ${pair.second}",  // accessibility: for screen readers to correctly pronounce each generated word pair
         ),
       ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // throw UnimplementedError();
+
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          )
+      ],
     );
   }
 }
