540061142ff910f611dba361e3056cb6ed96dcfd
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2024-08-15 12:43:06 +0800

    Make the app prettier

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(0, 255, 0, 1.0)),  // alternatively, Colors.deepOrange, etc.
         ),
         home: MyHomePage(),
       ),
@@ -38,20 +38,55 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea from SEGS:'),
-          Text(appState.current.asSnakeCase),
+      body: Center(  // wrap to horizontally center
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,  // to vertically center
+          children: [
+            Text('A random AWESOME idea from SEGS:'),
+            BigCard(pair: pair),
+            SizedBox(height: 10),  // visual spacing between BigCard and ElevatedButton
+            ElevatedButton(
+              onPressed: () {
+                // print('button pressed')
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-          ElevatedButton(
-            onPressed: () {
-              // print('button pressed')
-              appState.getNext();
-            },
-            child: Text('Next'))
-        ],
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary  // defines a color (for the text in this case) that is a good fit for use on the app's primary color
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      elevation: 5,  // shadow of the BigCard
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asSnakeCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",  // accessibility: for screen readers to correctly pronounce each generated word pair
+        ),
       ),
     );
   }
